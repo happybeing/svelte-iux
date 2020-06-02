d1a9a588cc7301cf60ad1798342f9b9dac6d7581
Mark <git@happybeing.com>
2020-06-02 18:26:04 +0100

    Update component exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-export { default as IUXContainer } from './IUXContainer.svelte'
-export { default as IUXRevealArea } from './IUXRevealArea.svelte'
+export { default as IUXTrayContainer } from './IUXTrayContainer.svelte'
+export { default as IUXTray } from './IUXTray.svelte'
+export { default as IUXTrayButton } from './IUXTrayButton.svelte'
 export { default as IUXRevealContainer } from './IUXRevealContainer.svelte'
-export { default as IUXTabContainer } from './IUXTabContainer.svelte'
+export { default as IUXRevealArea } from './IUXRevealArea.svelte'
