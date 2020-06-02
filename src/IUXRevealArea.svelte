e0e82ba247d366da0dc73518bc725b86a5f863c6
Mark <git@happybeing.com>
2020-06-02 16:21:01 +0100

    Add test versions of IUXRevealArea components

--- src/IUXRevealArea.svelte
+++ src/IUXRevealArea.svelte
@@ -1,25 +1,69 @@
 <script>
-  export let red = false
-  export let strong = false
+// A simple reveal content box, using Svelte transforms.
+//
+// Based on REPL:
+//   https://svelte.dev/repl/d32ecc5d5a6847148fb85acd44b93640?version=3.23.0
+//
+// This is sveltified version of RevealBoxJS.svelte which does the same thing
+// but using JavaScript.
+
+import { tick } from 'svelte';
+import { tweened } from 'svelte/motion';
+import { cubicInOut } from 'svelte/easing';
+
+export let reveal = false;
+let box;
+
+export let timePerPixel = 0.02;
+
+let boxStyle = '';
+let styleHide = `height: 0px; transition: height 0.8s ease-in;`;
+
+$: boxContentHeight = box ? box.scrollHeight : 0;
+$: styleReveal = `height: ${boxContentHeight}px; transition: height 0.8s ease-out;`;
+let styleRevealDone = `height: auto;`;
+
+$: revealOrHide(reveal);
+
+// Note: 'await tick()' allows each change to take effect before making another
+async function revealOrHide(reveal) {
+  console.log('revealOrHide: ', reveal);
+  if (!box) return;
+
+  if (reveal) {
+    box.style.height = '0px';
+    boxStyle = styleReveal;
+    await tick();
+  } else {
+    const boxTransition = box.style.transition;
+    box.style.transition = '';
+    await tick();
+    box.style.height = '' + box.scrollHeight + 'px';
+    box.style.transition = boxTransition;
+    await tick();
+    boxStyle = styleHide;
+  }
+}
+
+function onTransitionEnd () {
+    // remove "height" from inline styles so it can return to its initial value
+    console.log('ontransitionend()')
+    if (reveal) {
+      console.log('setting auto');
+      box.style.height = 'auto';
+    }
+}
+
 </script>
 
-<div class:red class:strong on:click>
-  <p>
-    <slot>I am IUXRevealArea.</slot>
-  </p>
-  <p>(click me)</p>
-</div>
-
 <style>
-  p {
-    font-family: sans-serif;
-    padding: .5em 1em;
-    text-align: center;
-  }
-  .red {
-    color: red;
-  }
-  .strong {
-    font-weight: bold;
-  }
+.reveal-box {
+  overflow: hidden;
+  height: auto;
+}
+
 </style>
+
+<div class='reveal-box' bind:this={box} style={boxStyle} on:transitionend={onTransitionEnd} >
+  <slot></slot>
+</div>
---
+++ src/IUXRevealAreaJS.svelte
@@ -0,0 +1,84 @@
+<script>
+// A simple reveal content box, using JavaScript to do the transform.
+//
+// It uses JavaScript because you can't use a CSS transition based on non absolute
+// property values (e.g. 'height: auto').
+//
+// The technique used is from https://css-tricks.com/using-css-transitions-auto-dimensions/#comments
+// with minor changes.
+//
+// Other techniques are to transition from fixed values (e.g. setting max-height), or calculating
+// the element height from the containered elements. Another may be possible with flexbox/flex-grow
+// but I've not looked into that yet, and there were more ideas in the article comments.
+
+export let reveal = false;
+let revealBox;
+
+
+function doHide(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  var contentHeight = element.scrollHeight;
+  
+  var elementTransition = element.style.transition;  // disable all css transitions
+  element.style.transition = '';
+  
+  // When the above style change has taken set the height to transition from
+  requestAnimationFrame(function() {
+    element.style.height = contentHeight + 'px';
+    element.style.transition = elementTransition;
+    
+    // When the above has taken effect, transition to height: 0
+    requestAnimationFrame(function() {
+      element.style.height = 0 + 'px';
+    });
+  });
+  
+  element.setAttribute('reveal-box-hidden', 'true');
+}
+
+function doReveal(element) {
+  var contentHeight = element.scrollHeight;
+  
+  element.style.height = contentHeight + 'px';
+
+  element.addEventListener('transitionend', function(e) {
+    // remove this event listener so it only gets triggered once
+    element.removeEventListener('transitionend', arguments.callee);    
+    // remove "height" from the element's inline styles, so it can return to its initial value
+    element.style.height = null;
+  });
+  
+  element.setAttribute('reveal-box-hidden', 'false');
+}
+
+$: revealOrHide(reveal);
+
+function revealOrHide(reveal) {
+  var box = document.querySelector('.reveal-box');
+  if (box) {
+    var isHidden = revealBox.getAttribute('reveal-box-hidden') === 'true';
+    
+    if(isHidden && reveal) {
+      doReveal(box)
+      revealBox.setAttribute('reveal-box-hidden', 'false')
+    } else if (!isHidden && !reveal) {
+      doHide(box);
+    }
+  }
+}
+</script>
+
+<style>
+.reveal-box {
+  /* border: 1px solid red;
+  background-color: #fde66d; */
+
+  overflow:hidden;
+  transition: height 0.8s ease-out;
+  height:auto;
+}
+</style>
+
+<div class='reveal-box' bind:this={revealBox} >
+  <slot></slot>
+</div>
