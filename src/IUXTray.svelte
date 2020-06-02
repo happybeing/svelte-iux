8afce6b6f5ea7ca4e32ba28a12eb0d710a32fe6c
Mark <git@happybeing.com>
2020-06-02 18:25:41 +0100

    Revise and implement IUX classes using tray/reveal components

--- src/IUXTray.svelte
+++ src/IUXTray.svelte
@@ -1,5 +1,5 @@
 <script>
-import TrayButton from './TrayButton.svelte';
+import IUXTrayButton from './IUXTrayButton.svelte';
 
 export let trayIn = true;
 
@@ -47,7 +47,7 @@ $: traySlideStyle = (trayIn ? trayInStyle : trayOutStyle) + trayStyle;
 
 <div class='tray' width='100%' style={traySlideStyle}>
   <div class='tray-content'><slot></slot></div>
-  <div class='top-right'><TrayButton height={buttonSize} width={buttonSize} bind:pointDown={trayIn} /></div>
+  <div class='top-right'><IUXTrayButton height={buttonSize} width={buttonSize} bind:pointDown={trayIn} /></div>
 </div>
 
 
---
+++ src/IUXTrayButton.svelte
@@ -0,0 +1,41 @@
+<!-- An animated button for opening and closing a container 'tray'
+
+The idea is for a down arrown (open tray) which animates to become an up 
+arrow (close tray) at during the animation of the tray sliding out, and
+vice versa.
+
+-->
+<script>
+export let width = 14;
+export let height = 12;
+
+export let pointDown = true;
+
+export let fill = '#65b81d';
+export let stroke = '#ffd700';
+export let strokeWidth = 1;
+
+$: colourStyles = `fill: ${fill}; 
+                    stroke: ${stroke}; 
+                    stroke-width: ${strokeWidth}; 
+                    width: ${width}; 
+                    height: ${height};`;
+
+$: triangleStyle = colourStyles + `
+                      margin: 1 auto; padding:1;
+                      width: ${width+strokeWidth}; 
+                      height: ${height};`;
+
+</script>
+
+<style>
+</style>
+
+<!-- Note: including on:click without a handler forwards the event to the component so it can be handled by the component user -->
+<div style="position: absolute; top: -2px; right: 0px; padding-right: 2px; height: {height+height/2+4}px;" on:click on:click={() => pointDown = !pointDown}>
+  <svg width={width} height={height+height/2}>
+    <polygon
+      points='{strokeWidth},{-height/2}, {width},{-height/2} {(width+strokeWidth)/2},{height/2}' 
+      style="{triangleStyle} transform: translate(0px, {height}px); transition: transform 0.8s;{pointDown ? '' : 'transform: translate(0px,' + height + 'px) rotateX(180deg)'}" />
+  </svg>
+</div>
