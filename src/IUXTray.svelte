3a12c1d9b0294cf9474242f38fb2fd80875f24b2
Mark <git@happybeing.com>
2020-06-06 18:19:23 +0100

    Add customisations to IUXTray/IUXTrayJS for header and button label

--- src/IUXTray.svelte
+++ src/IUXTray.svelte
@@ -2,20 +2,28 @@
 import IUXTrayButton from './IUXTrayButton.svelte';
 import IUXRevealArea from './IUXRevealArea.svelte';
 
-export let trayIn = true;
+// TODO: extend headingElement using <svelte:element> if implemented (see https://github.com/sveltejs/svelte/issues/2324)
+// TODO: ??? export let height;
 
-$: reveal = !trayIn;  // Controls the IUXRevealArea
-
-// export let height;// TODO?
-const minProtusion = 24;
+const minProtrusion = 24;
 const buttonSpacing = 10;
 
-export let protrudingHeight = minProtusion;
-export let buttonSize = minProtusion - buttonSpacing;
+export let reveal = false;
+
+export let heading = '';
+export let headingReveal = heading;
+export let headingElement = 'b';  // Support b, h1, h2, h3
+export let headingStyle = 'text-align: center; ';
+
+export let buttonLabel = '';
+export let buttonLabelWhenReveal = buttonLabel;
+
+export let protrudingHeight = minProtrusion;
+export let buttonSize = minProtrusion - buttonSpacing;
 export let trayStyle = '';
 
-$: useButtonSize = Math.max(buttonSize, minProtusion - buttonSpacing);
-$: minHeight = Math.max(buttonSize + buttonSpacing, minProtusion);
+$: useButtonSize = Math.max(buttonSize, minProtrusion - buttonSpacing);
+$: minHeight = Math.max(buttonSize + buttonSpacing, minProtrusion);
 
 </script>
 
@@ -42,11 +50,37 @@ $: minHeight = Math.max(buttonSize + buttonSpacing, minProtusion);
     margin: 0px;
     padding: 0px;
   }
+
 </style>
 
-<IUXRevealArea minHeight={minHeight} reveal={reveal}>
+<IUXRevealArea minHeight={protrudingHeight} reveal={reveal}>
   <div class='tray' style={trayStyle}>
-    <div class='top-right'><IUXTrayButton height={useButtonSize} width={useButtonSize} bind:pointDown={trayIn} /></div>
+    <div style={'display: flex; flex-direction: row-reverse; min-height: ' + protrudingHeight + 'px; width: 100%;'}>
+      
+      <div style={'display: block;height: ' + protrudingHeight + 'px; '}>
+        <div style={'width: 100%; height: ' + (protrudingHeight/2-16) + 'px; '}></div>
+        <div style={'float: right; width: min-content; height: ' + protrudingHeight/2 + 'px; '}>
+          <IUXTrayButton height={useButtonSize} width={useButtonSize} bind:pointUp={reveal} />
+        </div>
+        <div style={'float: right; width: min-content; height: ' + protrudingHeight/2 + 'px; padding-right: 0.3em;'}>
+          {!reveal ? buttonLabel : buttonLabelWhenReveal}
+        </div>
+      </div>
+      
+      <div style={'height: ' + protrudingHeight + 'px; flex-grow: 1; ' + headingStyle}>
+        {#if headingElement === 'b'}
+          <b>{!reveal ? heading : headingReveal}</b>
+        {:else if headingElement === 'h1'}
+          <h1>{!reveal ? heading : headingReveal}</h1>
+        {:else if headingElement === 'h2'}
+          <h2>{!reveal ? heading : headingReveal}</h2>
+        {:else if headingElement === 'h3'}
+          <h3>{!reveal ? heading : headingReveal}</h3>
+        {:else}
+          {!reveal ? heading : headingReveal}
+        {/if}
+       </div>
+    </div>
     <slot></slot>
   </div>
 </IUXRevealArea>
