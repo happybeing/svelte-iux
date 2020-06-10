aad33f1b3f6c0e4b735dd6801b569d4edda15d1a
Mark <git@happybeing.com>
2020-06-10 16:06:44 +0100

    Remove debug styles from IUXTrayHolder

--- src/IUXTrayHolder.svelte
+++ src/IUXTrayHolder.svelte
@@ -5,13 +5,9 @@
   .tray-holder {
     position: relative;
     width: 100%;
-    border: 1px solid black;
-    background-color: rgba(135, 199, 236, 1)
   }
 </style>
 
 <div class='tray-holder' >
   <slot></slot>
 </div>
-
-
