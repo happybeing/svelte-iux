4edbdece661325241c981e4662175216d7022fb1
Mark <git@happybeing.com>
2020-06-19 18:13:21 +0100

    Add 'disabled' feature to IUXTray/IUXTrayJS

--- src/IUXTray.svelte
+++ src/IUXTray.svelte
@@ -9,10 +9,11 @@ const minProtrusion = 24;
 const buttonSpacing = 10;
 
 export let reveal = false;
+export let disabled = false;
 
 export let heading = '';
 export let headingReveal = '';
-export let headingElement = 'b';  // Support b, h1, h2, h3
+export let headingElement = 'b';  // Supports b, h1, h2, h3
 export let headingStyle = 'text-align: center; ';
 
 export let buttonLabel = '';
@@ -27,38 +28,70 @@ $: buttonLabelReveal = buttonLabelReveal === '' ? buttonLabel : buttonLabelRevea
 $: useButtonSize = Math.max(buttonSize, minProtrusion - buttonSpacing);
 $: minHeight = Math.max(buttonSize + buttonSpacing, minProtrusion);
 
+let disableDuration = 2;  // Duration of disabled/enabled transitions
 </script>
 
 <style>
-  .tray {
-    position: relative;
-    width: 100%;
-    top: 0px;
-    overflow: hidden;
-  }
+.tray {
+  position: relative;
+  width: 100%;
+  top: 0px;
+  overflow: hidden;
+}
 
-  .tray-content {
-    position: absolute;
-    top: 0px;
-    overflow: hidden;
-    height: auto;
-    width: inherit;
-  }
+.tray-content {
+  position: absolute;
+  top: 0px;
+  overflow: hidden;
+  height: auto;
+  width: inherit;
+}
 
-  .top-right {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-    margin: 0px;
-    padding: 0px;
-  }
+.top-right {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  margin: 0px;
+  padding: 0px;
+}
+
+.dimmer {
+  width: 100%; 
+  height: 100%; 
+  position: absolute;
+}
+
+svg.dimmer {
+  height: auto;
+}
+
+rect.dimmer {
+  z-index: 100;
+  fill: gray;
+}
+
+rect.dimmer-inactive {
+  opacity: 0;
+  transition: opacity 1.5s;
+}
+
+rect.dimmer-active {
+  opacity: 0.2;
+  transition: opacity 1.5s;
+}
 
 </style>
 
 <IUXRevealArea minHeight={protrudingHeight} reveal={reveal}>
-  <div class='tray' style={trayStyle}>
+  <div {disabled} class='tray' style={trayStyle}>
+
+    <div class='dimmer' style={disabled ? '' : 'pointer-events: none;'}>
+      <svg class='dimmer' viewBox='0 0 100 100'>
+        <rect class={'dimmer ' + (disabled ? 'dimmer-active' : 'dimmer-inactive')}/>
+      </svg>
+    </div>
+
     <div style={'display: flex; flex-direction: row-reverse; min-height: ' + protrudingHeight + 'px; width: 100%;'}>
-      
       <div style={'cursor: pointer; display: block;height: ' + protrudingHeight + 'px; '}>
         <div style={'width: 100%; height: ' + (protrudingHeight/2-16) + 'px; '}></div>
         <div style={'float: right; width: min-content; height: ' + protrudingHeight/2 + 'px; '}>
@@ -81,8 +114,10 @@ $: minHeight = Math.max(buttonSize + buttonSpacing, minProtrusion);
         {:else}
           {!reveal ? heading : headingReveal}
         {/if}
-       </div>
+      </div>
     </div>
+
     <slot></slot>
+
   </div>
 </IUXRevealArea>
